f7266178e27d7ce4a09e2dd5e10de0e33a65b3e3
Ondřej Cífka <cifkao@users.noreply.github.com>
2025-07-17 18:01:42 +0000

    Make tree-sitter executable

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -4,3 +4,4 @@ uv tool install pyright
 uv tool install ruff
 
 wget https://github.com/tree-sitter/tree-sitter/releases/download/v0.25.3/tree-sitter-linux-x64.gz -O $HOME/.local/bin/tree-sitter
+chmod +x $HOME/.local/bin/tree-sitter
